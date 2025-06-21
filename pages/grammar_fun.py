fc404cced2208f2411b88f0f384abefa1109e340
naren <narender.1898@gmail.com>
2025-06-21 17:57:31 +0530

    updated openapikey

--- pages/grammar_fun.py
+++ pages/grammar_fun.py
@@ -3,9 +3,11 @@ import openai
 import config
 from openai import OpenAI
 #client = OpenAI(api_key=config.API_KEY)
+
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 def generate_grammar_exercise():
+    print(st.secrets["OPENAI_API_KEY"])
     ## Using OpenAI to generate a grammar exercise
     completion = client.chat.completions.create(
       model="gpt-3.5-turbo",
